34afc53a5a99ff1250e24b88995b7324dcb3a1b7
hppRC <hpp.ricecake@gmail.com>
2020-04-24 16:37:40 +0900

    :+1: add yes_no function

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,2 +1,14 @@
 pub mod digit;
 pub use digit::*;
+
+/// if true:
+///     return "Yes"
+/// if false:
+///     return "No"
+pub fn yes_no(judge: bool) -> &'static str {
+    if judge {
+        "Yes"
+    } else {
+        "No"
+    }
+}
