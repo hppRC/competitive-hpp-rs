2c9869e889733deb143f888aa719b60d190c5c8c
hppRC <hpp.ricecake@gmail.com>
2020-04-24 16:38:35 +0900

    :+1: change function name

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -5,7 +5,8 @@ pub use digit::*;
 ///     return "Yes"
 /// if false:
 ///     return "No"
-pub fn yes_no(judge: bool) -> &'static str {
+#[allow(non_snake_case)]
+pub fn YesNo(judge: bool) -> &'static str {
     if judge {
         "Yes"
     } else {
