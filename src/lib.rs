787bf4947129a61c06047ccdb3d269a7b11f1027
hppRC <hpp.ricecake@gmail.com>
2020-05-06 14:08:52 +0900

    :+1: Move definitions of constants from prelude to specific module

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod constants;
 pub mod dijkstra;
 pub mod modulo;
 pub mod prime;
--- src/prelude.rs
+++ src/prelude.rs
@@ -15,24 +15,8 @@ pub use num::integer::*;
 
 pub use num::*;
 
-pub const ALPHABETS: [char; 26] = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-    't', 'u', 'v', 'w', 'x', 'y', 'z',
-];
-
-pub const DIRE: [(isize, isize); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
-pub const DIRE8: [(isize, isize); 8] = [
-    (1, 0),
-    (1, 1),
-    (0, 1),
-    (-1, 1),
-    (-1, 0),
-    (-1, -1),
-    (0, -1),
-    (1, -1),
-];
-
 // re-exports
+pub use crate::constants::*;
 pub use crate::dijkstra::Dijkstra;
 pub use crate::modulo::*;
 pub use crate::prime::*;
