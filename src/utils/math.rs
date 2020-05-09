8ce7173c34d05716c8b6d0dda8987e66329770c1
hppRC <hpp.ricecake@gmail.com>
2020-05-10 02:30:27 +0900

    :+1: Add sqrt functions

--- src/utils/math.rs
+++ src/utils/math.rs
@@ -7,6 +7,8 @@ pub trait MathUtils {
     /// assert_eq!(10.log2_trunc(), 3);
     /// ```
     fn log2_trunc(self) -> Self;
+    fn sqrt_floor(self) -> Self;
+    fn sqrt_ceil(self) -> Self;
 }
 
 macro_rules! impl_digit_utils(($($ty:ty),*) => {
@@ -14,6 +16,19 @@ macro_rules! impl_digit_utils(($($ty:ty),*) => {
         impl MathUtils for $ty {
             fn log2_trunc(self) -> Self {
                 (self as f64).log2().trunc() as Self
+            }
+
+            fn sqrt_floor(self) -> Self {
+                (self as f64).sqrt() as Self
+            }
+
+            fn sqrt_ceil(self) -> Self {
+                let tmp = (self as f64).sqrt() as Self;
+                if tmp * tmp == self {
+                    tmp
+                } else {
+                    tmp + 1
+                }
             }
         }
     )*
@@ -28,6 +43,15 @@ mod tests {
     macro_rules! impl_math_tests (($ty:ty) => {
         assert_eq!((16 as $ty).log2_trunc(), 4);
         assert_eq!((10 as $ty).log2_trunc(), 3);
+
+        assert_eq!((9 as $ty).sqrt_floor(), 3);
+        assert_eq!((10 as $ty).sqrt_floor(), 3);
+        assert_eq!((15 as $ty).sqrt_floor(), 3);
+
+        assert_eq!((9 as $ty).sqrt_ceil(), 3);
+        assert_eq!((10 as $ty).sqrt_ceil(), 4);
+        assert_eq!((10 as $ty).sqrt_ceil(), 4);
+
     });
 
     #[test]
