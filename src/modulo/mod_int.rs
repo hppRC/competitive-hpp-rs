9b9b40af5defc268f7a57152d7002efc0e779623
hppRC <hpp.ricecake@gmail.com>
2020-05-15 21:56:26 +0900

    :sparkles: Add usefull ModInt

--- src/modulo/mod_int.rs
+++ src/modulo/mod_int.rs
@@ -1,33 +1,69 @@
-use num::traits::{NumOps, PrimInt};
+use num::integer::*;
+use num::traits::{NumOps, PrimInt, ToPrimitive};
+use std::cmp::Ordering;
 use std::fmt;
 use std::ops::{Add, Div, Mul, Sub};
 
+/// ## Example
+/// ```
+/// use competitive_hpp::prelude::*;
+/// const MOD: usize = 7;
+///
+/// let mi0 = ModInt::new_with(0, MOD);
+/// let mi1 = ModInt::new_with(1, MOD);
+/// let mi2 = ModInt::new_with(2, MOD);
+/// let mi11 = ModInt::new_with(11, MOD);
+/// assert_eq!(4, ModInt::new(4));
+/// assert_eq!(mi0 + mi11, ModInt::new_with(4, 7));
+/// assert_eq!(mi1 + mi2, ModInt::new_with(3, 7));
+/// ```
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct ModInt<T> {
-    value: T,
-    modulo: T,
+    pub value: T,
+    pub modulo: T,
 }
 
-trait ModIntTrait<T>
-where
-    T: PrimInt,
-{
-    fn new(n: T, modulo: T) -> Self;
-    fn inverse() -> Self;
+pub trait ModIntTrait<T> {
+    fn new(n: T) -> Self;
+    fn new_with(n: T, modulo: T) -> Self;
+    fn inverse(&self) -> Self;
+    fn static_inverse_with(n: T, modulo: T) -> T;
 }
 
 impl<T> ModIntTrait<T> for ModInt<T>
 where
     T: PrimInt,
 {
-    fn new(n: T, modulo: T) -> Self {
+    fn new(n: T) -> Self {
+        Self::new_with(n, T::from(1000000007).unwrap())
+    }
+
+    fn new_with(n: T, modulo: T) -> Self {
         ModInt {
             value: n % modulo,
             modulo,
         }
     }
-    fn inverse() -> Self {
-        todo!()
+
+    #[inline]
+    fn inverse(&self) -> Self {
+        let value = Self::static_inverse_with(self.value, self.modulo);
+        ModInt {
+            value,
+            modulo: self.modulo,
+        }
+    }
+
+    #[inline]
+    fn static_inverse_with(n: T, modulo: T) -> T {
+        let ExtendedGcd { x, .. } = n.to_i64().unwrap().extended_gcd(&modulo.to_i64().unwrap());
+
+        T::from(if x < 0 {
+            x + modulo.to_i64().unwrap()
+        } else {
+            x
+        })
+        .unwrap()
     }
 }
 
@@ -46,6 +82,7 @@ where
 {
     type Output = ModInt<T>;
 
+    #[inline]
     fn add(self, other: ModInt<T>) -> Self {
         ModInt {
             value: if self.value + other.value >= self.modulo {
@@ -64,6 +101,7 @@ where
 {
     type Output = ModInt<T>;
 
+    #[inline]
     fn add(self, rhs: T) -> Self {
         ModInt {
             value: if self.value + rhs >= self.modulo {
@@ -84,6 +122,7 @@ macro_rules! impl_modint_add(($($ty:ty),*) => {
         {
             type Output = ModInt<T>;
 
+            #[inline]
             fn add(self, rhs: ModInt<T>) -> ModInt<T> {
                 ModInt {
                     value: if T::from(self).unwrap() + rhs.value >= rhs.modulo {
@@ -98,7 +137,7 @@ macro_rules! impl_modint_add(($($ty:ty),*) => {
     )*
 });
 
-impl_modint_add!(i32, i64, u32, u64, isize, usize);
+impl_modint_add!(i8, i16, i32, i64, u8, u16, u32, u64, isize, usize);
 
 impl<T> Sub for ModInt<T>
 where
@@ -106,6 +145,7 @@ where
 {
     type Output = ModInt<T>;
 
+    #[inline]
     fn sub(self, other: ModInt<T>) -> Self {
         ModInt {
             value: if self.value < other.value {
@@ -124,6 +164,7 @@ where
 {
     type Output = ModInt<T>;
 
+    #[inline]
     fn sub(self, rhs: T) -> Self {
         ModInt {
             value: if self.value < rhs {
@@ -136,6 +177,212 @@ where
     }
 }
 
+macro_rules! impl_modint_sub(($($ty:ty),*) => {
+    $(
+        impl<T> Sub<ModInt<T>> for $ty
+        where
+            T: PrimInt,
+        {
+            type Output = ModInt<T>;
+
+            #[inline]
+            fn sub(self, rhs: ModInt<T>) -> ModInt<T> {
+                ModInt {
+                    value: if T::from(self).unwrap() < rhs.value {
+                        T::from(self).unwrap() + rhs.modulo - rhs.value
+                    } else {
+                        (T::from(self).unwrap() - rhs.value) % rhs.modulo
+                    },
+                    modulo: rhs.modulo,
+                }
+            }
+        }
+    )*
+});
+
+impl_modint_sub!(i8, i16, i32, i64, u8, u16, u32, u64, isize, usize);
+
+impl<T> Mul for ModInt<T>
+where
+    T: PrimInt,
+{
+    type Output = ModInt<T>;
+
+    #[inline]
+    fn mul(self, other: ModInt<T>) -> Self {
+        ModInt {
+            value: (self.value * other.value) % self.modulo,
+            modulo: self.modulo,
+        }
+    }
+}
+
+impl<T> Mul<T> for ModInt<T>
+where
+    T: PrimInt,
+{
+    type Output = ModInt<T>;
+
+    #[inline]
+    fn mul(self, rhs: T) -> Self {
+        ModInt {
+            value: (self.value * rhs) % self.modulo,
+            modulo: self.modulo,
+        }
+    }
+}
+
+macro_rules! impl_modint_mul(($($ty:ty),*) => {
+    $(
+        impl<T> Mul<ModInt<T>> for $ty
+        where
+            T: PrimInt,
+        {
+            type Output = ModInt<T>;
+
+            #[inline]
+            fn mul(self, rhs: ModInt<T>) -> ModInt<T> {
+                ModInt {
+                    value: (T::from(self).unwrap() * rhs.value) % rhs.modulo,
+                    modulo: rhs.modulo,
+                }
+            }
+        }
+    )*
+});
+
+impl_modint_mul!(i8, i16, i32, i64, u8, u16, u32, u64, isize, usize);
+
+impl<T> Div for ModInt<T>
+where
+    T: PrimInt,
+{
+    type Output = ModInt<T>;
+
+    #[inline]
+    fn div(self, other: ModInt<T>) -> Self {
+        ModInt {
+            value: (self.value * other.inverse().value) % self.modulo,
+            modulo: self.modulo,
+        }
+    }
+}
+
+impl<T> Div<T> for ModInt<T>
+where
+    T: PrimInt,
+{
+    type Output = ModInt<T>;
+
+    #[inline]
+    fn div(self, rhs: T) -> Self {
+        let inv = Self::static_inverse_with(rhs, self.modulo);
+        ModInt {
+            value: (self.value * inv) % self.modulo,
+            modulo: self.modulo,
+        }
+    }
+}
+
+macro_rules! impl_modint_div(($($ty:ty),*) => {
+    $(
+        impl<T> Div<ModInt<T>> for $ty
+        where
+            T: PrimInt,
+        {
+            type Output = ModInt<T>;
+
+            #[inline]
+            fn div(self, rhs: ModInt<T>) -> ModInt<T> {
+                let inv = ModInt::static_inverse_with(rhs.value, rhs.modulo);
+                ModInt {
+                    value: (T::from(self).unwrap() * inv) % rhs.modulo,
+                    modulo: rhs.modulo,
+                }
+            }
+        }
+    )*
+});
+
+impl_modint_div!(i8, i16, i32, i64, u8, u16, u32, u64, isize, usize);
+
+impl<T> PartialEq<T> for ModInt<T>
+where
+    T: PrimInt,
+{
+    fn eq(&self, other: &T) -> bool {
+        self.value == other.clone()
+    }
+}
+
+macro_rules! impl_modint_partial_eq(($($ty:ty),*) => {
+    $(
+        impl<T> PartialEq<ModInt<T>> for $ty
+        where
+            T: PrimInt,
+        {
+            #[inline]
+            fn eq(&self, other: &ModInt<T>) -> bool {
+                T::from(self.clone()).unwrap() == other.value.clone()
+            }
+        }
+    )*
+});
+
+impl_modint_partial_eq!(i8, i16, i32, i64, u8, u16, u32, u64, isize, usize);
+
+impl<T> PartialOrd<T> for ModInt<T>
+where
+    T: PrimInt,
+{
+    fn partial_cmp(&self, other: &T) -> Option<Ordering> {
+        Some(self.value.cmp(other))
+    }
+}
+
+macro_rules! impl_modint_partial_ord(($($ty:ty),*) => {
+    $(
+        impl<T> PartialOrd<ModInt<T>> for $ty
+        where
+            T: PrimInt,
+        {
+            #[inline]
+            fn partial_cmp(&self, other: &ModInt<T>) -> Option<Ordering> {
+                Some(T::from(self.clone()).unwrap().cmp(&other.value))
+            }
+        }
+    )*
+});
+
+impl_modint_partial_ord!(i8, i16, i32, i64, u8, u16, u32, u64, isize, usize);
+
+macro_rules! impl_modint_to_primitive(($(($ty:ty, $method:ident)),*) => {
+    $(
+        #[inline]
+        fn $method(&self) -> Option<$ty> {
+            self.value.$method()
+        }
+    )*
+});
+
+impl<T> ToPrimitive for ModInt<T>
+where
+    T: PrimInt,
+{
+    impl_modint_to_primitive!(
+        (i8, to_i8),
+        (i16, to_i16),
+        (i32, to_i32),
+        (i64, to_i64),
+        (u8, to_u8),
+        (u16, to_u16),
+        (u32, to_u32),
+        (u64, to_u64),
+        (isize, to_isize),
+        (usize, to_usize)
+    );
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -143,26 +390,90 @@ mod test {
     #[test]
     fn test_modint_modint() {
         const MOD: usize = 7;
-        let mi0 = ModInt::new(0, MOD);
-        let mi1 = ModInt::new(1, MOD);
-        let mi2 = ModInt::new(2, MOD);
-        let mi4 = ModInt::new(4, MOD);
-        let mi7 = ModInt::new(7, MOD);
-        let mi11 = ModInt::new(11, MOD);
+        let mi0 = ModInt::new_with(0, MOD);
+        let mi1 = ModInt::new_with(1, MOD);
+        let mi2 = ModInt::new_with(2, MOD);
+        let mi4 = ModInt::new_with(4, MOD);
+        let mi7 = ModInt::new_with(7, MOD);
+        let mi11 = ModInt::new_with(11, MOD);
 
-        assert_eq!(mi0 + mi7, ModInt::new(0, 7));
-        assert_eq!(mi1 + mi2, ModInt::new(3, 7));
-        assert_eq!(mi1 + mi11, ModInt::new(5, 7));
-        assert_eq!(mi1 - mi4, ModInt::new(4, 7));
+        assert_eq!(mi0 + mi7, ModInt::new_with(0, 7));
+        assert_eq!(mi1 + mi2, ModInt::new_with(3, 7));
+        assert_eq!(mi1 + mi11, ModInt::new_with(5, 7));
+        assert_eq!(mi1 - mi4, ModInt::new_with(4, 7));
     }
 
     #[test]
     fn test_modint_other_type() {
         const MOD: usize = 7;
-        let mi0 = ModInt::new(0, MOD);
+        let mi0 = ModInt::new_with(0, MOD);
 
-        assert_eq!(mi0 + 6, ModInt::new(6, 7));
-        assert_eq!(mi0 + 7, ModInt::new(0, 7));
-        assert_eq!(7usize + mi0, ModInt::new(0, 7));
+        assert_eq!(mi0 + 6, ModInt::new_with(6, MOD));
+        assert_eq!(mi0 + 7, ModInt::new_with(0, MOD));
+        assert_eq!(7usize + mi0, ModInt::new_with(0, MOD));
+        assert_eq!(15usize + mi0, ModInt::new_with(1, MOD));
+        assert_eq!(mi0 - 4, ModInt::new_with(3, MOD));
+        assert_eq!(mi0 - 0, ModInt::new_with(0, MOD));
+        assert_eq!(7usize - mi0, ModInt::new_with(0, MOD));
+    }
+
+    #[test]
+    fn test_new() {
+        let mi0 = ModInt::new(0);
+        let mi1 = ModInt::new(7);
+        let mi2 = ModInt::new(1000000007);
+
+        assert_eq!(mi0, ModInt::new(0));
+        assert_eq!(mi1 + mi2, ModInt::new(7));
+    }
+    #[test]
+    fn test_inverse() {
+        const MOD: u64 = 13;
+
+        assert_eq!(1, ModInt::new_with(1, MOD).inverse());
+        assert_eq!(7, ModInt::new_with(2, MOD).inverse());
+        assert_eq!(9, ModInt::new_with(3, MOD).inverse());
+        assert_eq!(10, ModInt::new_with(4, MOD).inverse());
+        assert_eq!(8, ModInt::new_with(5, MOD).inverse());
+        assert_eq!(11, ModInt::new_with(6, MOD).inverse());
+        assert_eq!(2, ModInt::new_with(7, MOD).inverse());
+        assert_eq!(5, ModInt::new_with(8, MOD).inverse());
+        assert_eq!(3, ModInt::new_with(9, MOD).inverse());
+        assert_eq!(4, ModInt::new_with(10, MOD).inverse());
+        assert_eq!(6, ModInt::new_with(11, MOD).inverse());
+        assert_eq!(12, ModInt::new_with(12, MOD).inverse());
+    }
+
+    #[test]
+    fn test_div() {
+        const MOD: u64 = 13;
+
+        assert_eq!(4, (ModInt::new_with(2, MOD) / ModInt::new_with(7, MOD)));
+        assert_eq!(4, (2u64 / ModInt::new_with(7, MOD)));
+        assert_eq!(4, (ModInt::new_with(2, MOD) / 7));
+    }
+
+    #[test]
+    fn test_mul() {
+        const MOD: u64 = 13;
+
+        assert_eq!(2, ModInt::new_with(3, MOD) * ModInt::new_with(5, MOD));
+        assert_eq!(2, ModInt::new_with(3, MOD) * 5);
+        assert_eq!(2, 3 * ModInt::new_with(5, MOD));
+    }
+
+    #[test]
+    fn test_partialord() {
+        const MOD: u64 = 13;
+
+        assert!(ModInt::new_with(3, MOD) < ModInt::new_with(5, MOD));
+        assert!(3 < ModInt::new_with(5, MOD));
+        assert!(ModInt::new_with(3, MOD) < 5);
+        assert!(!(ModInt::new(10) < 7));
+    }
+
+    #[test]
+    fn test_to_primitive() {
+        assert_ne!(2, ModInt::new(13).to_u64().unwrap());
     }
 }
